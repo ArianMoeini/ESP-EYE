0d01d24ddc6424fcb4ed60752a9946811b0470de
Arian <arianmoeini11@gmail.com>
2024-11-06 16:54:27 +0100

    xray model

---
+++ main/model/model.h
@@ -0,0 +1,11 @@
+#ifndef MODEL_H_
+#define MODEL_H_
+
+#include <stdint.h>
+
+// Declare the model array and its length
+extern const unsigned char cifar10_model_quant_tflite[];
+extern const unsigned int cifar10_model_quant_tflite_len;
+
+#endif  // MODEL_H_
+
