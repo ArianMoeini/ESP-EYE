4be5e9938900ea5a378a56ac6868fe14396317b4
Arian <arianmoeini11@gmail.com>
2024-11-06 16:53:11 +0100

    Remove unnecessary model files from Git tracking

--- main/model/smallscript.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-# Load the TFLite model
-interpreter = tf.lite.Interpreter(model_path="/Users/arianmoeini/Desktop/hardware/esp-idf/examples/arian/esp-eye-image-classification/main/model/model.c")
-operator_codes = interpreter._get_ops_list()
-
-print("Operators in the model:")
-for op in operator_codes:
-    print(op)
